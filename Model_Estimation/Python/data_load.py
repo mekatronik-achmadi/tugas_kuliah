1030bf70f43b4081b53ae43f267a0d1f47aafc8c
mekatronik-achmadi <mekatronik.achmadi@gmail.com>
2016-12-31 13:11:57 +0700

    toloooong

--- Model_Estimation/Python/data_load.py
+++ Model_Estimation/Python/data_load.py
@@ -1,37 +1,40 @@
-import scipy.io
+import scipy.io as sio
 import matplotlib.pyplot as plt
 
-input_data=scipy.io.loadmat("data_input.mat")['input_data']
-output_data=scipy.io.loadmat("data_output.mat")['output_data']
-
-u_11 = input_data[20:1450,1]
-u_12 = input_data[20:1450,3]
-u_13 = input_data[20:1450,2]
-
-y_11 = output_data[20:1450,1]
-y_12 = output_data[20:1450,2]
-
-u11 = u_11/u_11.max()
-u12 = u_12/u_12.max()
-u13 = u_13/u_13.max()
-y11 = y_11/y_11.max()
-y12 = y_12/y_12.max()
-
-plt.subplot(5, 1, 1)
-plt.plot(u11)
-plt.ylabel('Input 1')
-plt.subplot(5, 1, 2)
-plt.plot(u12)
-plt.ylabel('Input 2')
-plt.subplot(5, 1, 3)
-plt.plot(u13)
-plt.ylabel('Input 3')
-plt.subplot(5, 1, 4)
-plt.plot(y11)
-plt.ylabel('Output 1')
-plt.subplot(5, 1, 5)
-plt.plot(y12)
-plt.ylabel('Output 2')
-
-plt.xlabel('Data Series')
-plt.show()
+class DataLoad:
+    def __init__(self):
+        input_data=sio.loadmat("data_input.mat")['input_data']
+        output_data=sio.loadmat("data_output.mat")['output_data']
+        
+        u_11 = input_data[20:1450,1]
+        u_12 = input_data[20:1450,3]
+        u_13 = input_data[20:1450,2]
+        
+        y_11 = output_data[20:1450,1]
+        y_12 = output_data[20:1450,2]
+        
+        self.u11 = u_11/u_11.max()
+        self.u12 = u_12/u_12.max()
+        self.u13 = u_13/u_13.max()
+        self.y11 = y_11/y_11.max()
+        self.y12 = y_12/y_12.max()
+        
+    def plot(self):
+        plt.subplot(5, 1, 1)
+        plt.plot(self.u11)
+        plt.ylabel('Input 1')
+        plt.subplot(5, 1, 2)
+        plt.plot(self.u12)
+        plt.ylabel('Input 2')
+        plt.subplot(5, 1, 3)
+        plt.plot(self.u13)
+        plt.ylabel('Input 3')
+        plt.subplot(5, 1, 4)
+        plt.plot(self.y11)
+        plt.ylabel('Output 1')
+        plt.subplot(5, 1, 5)
+        plt.plot(self.y12)
+        plt.ylabel('Output 2')
+        
+        plt.xlabel('Data Series')
+        plt.show()
---
+++ Model_Estimation/Python/test_arma.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Dec 31 07:46:11 2016
+
+@author: achmadi
+"""
+
+import numpy as np
+from scipy import stats as st
+import pandas as pd
+import matplotlib.pyplot as plt
+import statsmodels.api as sm
+
+from data_load import DataLoad
+
+dat = DataLoad()
+#dat.plot()
+
+d = {'u11': dat.u11, 'u12': dat.u12}
+df = pd.DataFrame(data=d)
